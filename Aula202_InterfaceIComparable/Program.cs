f6b8759cae1e27facc4f9d3f97c5b1ebb77b7654
coelhovinicius <coelhovinicius@yahoo.com.br>
2022-01-04 19:28:59 -0300

    Interface IComparable

---
+++ Aula202_InterfaceIComparable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Aula202_InterfaceIComparable.Entities;
+
+namespace Aula202_InterfaceIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    // List<string> list = new List<string>();
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort(); // Ordena a lista
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
